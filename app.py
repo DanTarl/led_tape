7644135786ed4d1d8d42c6f6e1deaf0d2552d605
Dan Tarleton <danieltarleton@hotmail.co.uk>
2017-01-13 00:07:21 +0000

    Added slow fade

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 import pigpio
 import math
+import time
 # import sys
 
 app = Flask(__name__)
@@ -14,8 +15,15 @@ app = Flask(__name__)
 def set(pin, brightness):
     real_brightness = math.ceil(brightness * 2.55)
     pi = pigpio.pi()
-    current = pi.set_PWM_dutycycle(int(pin))
-    pi.set_PWM_dutycycle(int(pin), int(real_brightness))
+    current = math.ceil(pi.get_PWM_dutycycle(int(pin)))
+    if current < real_brightness:
+      for x in range(int(current), int(real_brightness)):
+        pi.set_PWM_dutycycle(int(pin), int(x))
+        time.sleep(0.1)
+    else:
+      for x in range(int(current), int(real_brightness), -1):
+        pi.set_PWM_dutycycle(int(pin), int(x))
+        time.sleep(0.1)
     pi.stop()
     return "Success"
 
